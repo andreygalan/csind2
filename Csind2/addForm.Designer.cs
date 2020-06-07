2889191c74e4233e8ec89c7ec6d5cf1332a66488
Andrey <galan.andrey@gmail.com>
2020-06-07 19:49:10 +0300

    v0.11.1

--- Csind2/addForm.Designer.cs
+++ Csind2/addForm.Designer.cs
@@ -89,7 +89,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(234, 136);
+            this.button1.Location = new System.Drawing.Point(449, 65);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 10;
